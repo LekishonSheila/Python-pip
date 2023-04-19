a2024eaaf43655354fd42e278c96c5398b7dc5af
lekishonsheila <lekishonsheila@gmail.com>
2023-04-19 19:14:23 +0300

    first

---
+++ functions.js
@@ -0,0 +1,27 @@
+// The radius of a circle is 30 meters
+// # calculate the area of a circle with radius of 30 meters
+radius = 30
+area_of_circle = math.pi * radius ** 2
+print("Area of circle with radius 30 meters:", area_of_circle)
+
+// # calculate the circumference of a circle with radius of 30 meters
+circum_of_circle = 2 * math.pi * radius
+print("Circumference of circle with radius 30 meters:", circum_of_circle)
+
+// # calculate the area of a circle with user input radius
+user_radius = float(input("Enter the radius of the circle: "))
+area_of_circle = math.pi * user_radius ** 2
+print("Area of circle with radius", user_radius, "meters:", area_of_circle)
+
+// Use the built-in input function to get first name, last name, country and age from a user 
+// and store the value to their corresponding variable names
+first_name = input("Lekishon: ")
+last_name = input("Sheila: ")
+country = input("Seneyia: ")
+age = input("20: ")
+
+// # print the values of the variables
+print("First name:", first_name)
+print("Last name:", last_name)
+print("Country:", country)
+print("Age:", age)
---
+++ sheila.py
@@ -0,0 +1,11 @@
+fruits = ["mango","banana","apple"]
+fruits.append("avocado")
+print(fruits)
+fruits2 = ["pawpaw","orange","passion"]
+fruits.extend(fruits2)
+print(fruits)
+fruits.insert(0,"berries")
+print(fruits)
+school("AkiraChix")
+print(1,"e")
+fruits.sort()
